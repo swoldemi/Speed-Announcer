0a8d193343aa3de7a97794d5c2fbf4d3bd478f22
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2017-12-30 15:34:23 -0600

    Now Able to send authenticated Tweets

--- SendTweet.py
+++ SendTweet.py
@@ -1,23 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
- 
-import tweepy, time, sys
- 
-argfile = str(sys.argv[1])
- 
-#enter the corresponding information from your Twitter application:
-CONSUMER_KEY = '1234abcd...'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = '1234abcd...'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '1234abcd...'#keep the quotes, replace this with your access token
-ACCESS_SECRET = '1234abcd...'#keep the quotes, replace this with your access token secret
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
- 
-filename=open(argfile,'r')
+import time, sys
+from twython import Twython
+
+# Load credentials
+filename=open('credentials.txt','r')
 f=filename.readlines()
 filename.close()
- 
-for line in f:
-    api.update_status(line)
-    time.sleep(900)#Tweet every 15 minutes
+
+# Authenticate with Twitter
+APP_KEY = f[0].split('\n')[0]
+APP_SECRET = f[1].split('\n')[0]
+OAUTH_TOKEN = f[2].split('\n')[0]
+OAUTH_TOKEN_SECRET = f[3].split('\n')[0]
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+# Send a Tweet
+twitter.update_status(status="I just started using Twython!")
+#time.sleep(900)#Tweet every 15 minutes
