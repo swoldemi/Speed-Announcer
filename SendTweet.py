72f618df6c80ba5b7c7b3c21663cfee3723696d0
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2017-12-30 20:27:59 -0600

    Now able to send a properly parsed Tweet

--- SendTweet.py
+++ SendTweet.py
@@ -1,4 +1,4 @@
-import time, sys
+import time, subprocess, re
 from twython import Twython
 
 # Load credentials
@@ -13,6 +13,24 @@ OAUTH_TOKEN = f[2].split('\n')[0]
 OAUTH_TOKEN_SECRET = f[3].split('\n')[0]
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
+
+# Conduct a speed test 
+speedtest_result = subprocess.getoutput('speedtest-cli')
+
+# Parse the result
+pattern = re.compile("\d+[.]\d+")
+num_values = pattern.findall(speedtest_result)
+server = speedtest_result.split("(")[2].split(")")[0]
+isp = "XFINITY by Comcast"
+tweet = """
+Speedtest.net Results for ISP {}:
+Test server: Hosted by ScaleMatrix - ({})[{} km]
+Ping: {} ms
+Download Speed: {} Mbit/s
+Upload Speed: {} Mbit/s
+""".format(isp, server, num_values[2], num_values[3], num_values[4], num_values[5])
+
+
 # Send a Tweet
-twitter.update_status(status="I just started using Twython!")
+twitter.update_status(status=tweet)
 #time.sleep(900)#Tweet every 15 minutes
