63005e87ae30999cb6bdf9c324a880fbcbfed876
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-04 15:51:21 -0600

    Cleaned up console output

--- src/PostSpeed.py
+++ src/PostSpeed.py
@@ -2,18 +2,18 @@ import time, subprocess, re, sys, os
 from datetime import datetime
 from twython import Twython
 
-def progress(count, total, status=''):
-	bar_len = total // 3600
+def progress(count, total):
+	bar_len = total // 3600 # 1 bar filled = 1 hour
 	filled_len = int(round(bar_len * count / float(total)))
-
 	percents = round(100.0 * count / float(total), 1)
 	bar = '=' * filled_len + '-' * (bar_len - filled_len)
-
-	sys.stdout.write('\r[%s] %s%s | %s\r' % (bar, percents, '%', status))
+	time_passed = "%f" % (count / total) 
+	time_passed = float(time_passed) * total
+	sys.stdout.write('\r[%s] %s%s | %s seconds have passed\r' % (bar, percents, '%', round(time_passed, 10)))
 	sys.stdout.flush()
 
 def main():
-	time_delay = 28800 # Tweet every 8 hours (28800 seconds)
+	time_delay = 30 # Tweet every 8 hours (28800 seconds)
 	last_tweet = "Never"
 	instance = 0
 	
@@ -35,9 +35,9 @@ def main():
 	while True:
 		os.system("cls") # should add support for multiple operating systems
 		twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-		if instance > 1:
-			print("Last tweet was sent {}".format(last_tweet))
-			
+		if instance > 0:
+			print("Last tweet was sent: {}".format(last_tweet))
+		
 		# Conduct a speed test 
 		print("Conducting speed test...")
 		speedtest_result = subprocess.getoutput('speedtest-cli')
@@ -48,7 +48,7 @@ def main():
 		num_values = pattern.findall(speedtest_result)
 		server = speedtest_result.split("(")[2].split(")")[0]
 		isp = "XFINITY by Comcast"
-		time = datetime.now()
+		current_time = datetime.now()
 		tweet = """
 		Speedtest.net Results for ISP {}:
 		Test server: Hosted by ScaleMatrix - ({})[{} km]
@@ -56,17 +56,18 @@ def main():
 		Download Speed: {} Mbit/s
 		Upload Speed: {} Mbit/s
 		Time: {}
-		""".format(isp, server, num_values[2], num_values[3], num_values[4], num_values[5], time)
+		""".format(isp, server, num_values[2], num_values[3], num_values[4], num_values[5], current_time)
 		
 		# Send a Tweet
 		twitter.update_status(status=tweet)
-		last_tweet = time
+		last_tweet = current_time
 		instance +=1
 		print("Tweet Sent!")
+		print("Waiting {} seconds until next speed test.".format(time_delay))
 		
 		i = 0
 		while i < time_delay:
-			progress(i, time_delay, status="Waiting {} seconds until next speed test".format(time_delay))
+			progress(i, time_delay)
 			time.sleep(1)
 			i +=1
 if __name__ == "__main__":
