00672976c02b80e68e80f12cf718814ebd0153e0
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-04 15:04:26 -0600

    Updated to use default base

--- setup.py
+++ setup.py
@@ -2,16 +2,10 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os", "queue", "idna"], "excludes": ["tkinter"]}
-
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+build_exe_options = {"packages": ["os", "sys", "queue", "idna"], "excludes": ["tkinter"]}
 
 setup(  name = "Speed Announcer",
         version = "0.11",
         description = "A program to post my internet speed to Twitter",
         options = {"build_exe": build_exe_options},
-        executables = [Executable("src/PostSpeed.py", base=base)])
+        executables = [Executable("src/PostSpeed.py")])
